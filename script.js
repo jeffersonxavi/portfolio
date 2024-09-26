3b787098badd38ded50e6376b9e2dd1df03effab
jeffersonxavi <jefingbi2@gmail.com>
2024-09-26 14:51:37 -0300

    transicao e exibir icone de rolagem

--- script.js
+++ script.js
@@ -12,15 +12,48 @@ const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () => {
+link1.addEventListener('click', (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
     scrollToElement('.header');
 });
 
-link2.addEventListener('click', () => {
-    // Rolagem para o segundo elemento com classe "header"
+link2.addEventListener('click', (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
     scrollToElement('.header', 1);
 });
 
-link3.addEventListener('click', () => {
+link3.addEventListener('click', (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
     scrollToElement('.column');
+});
+
+//Transição de imagens com bolinhas de navegação: 
+const dots = document.querySelectorAll('.dot');
+const images = document.querySelectorAll('.screenshots img');
+let currentIndex = 0;
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        images[currentIndex].classList.remove('active');
+        dots[currentIndex].classList.remove('active');
+
+        currentIndex = index;
+
+        images[currentIndex].classList.add('active');
+        dots[currentIndex].classList.add('active');
+    });
+});
+
+//Exibição do ícone de rolagem
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollIcon = document.querySelector('.scroll-icon');
+    scrollIcon.style.opacity = '1'; // Garante que o ícone comece visível
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            scrollIcon.style.opacity = '0'; // Oculta o ícone ao rolar
+        } else {
+            scrollIcon.style.opacity = '1'; // Mostra o ícone no topo
+        }
+    });
 });
